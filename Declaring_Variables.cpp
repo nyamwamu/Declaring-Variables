480e067afa31db9102a7a4733cdf7516693325a1
Nyamwamu Vincent <nyamwamuvincent@gmail.com>
2018-12-19 10:49:26 +0300

    Add files via upload

---
+++ Declaring_Variables.cpp
@@ -0,0 +1,26 @@
+//Declaring Variables
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+
+{
+    string characterName = "Vincent";
+    int characterAge;
+    characterAge = 50;
+
+    cout << "There was once a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+
+    characterName = "Nyamwamu";
+
+    cout << "He liked the name " << characterName << endl;
+    cout << "But he did not like being " << characterAge <<endl;
+
+    characterAge = 20;
+
+    cout << "He wished he was " << characterAge << endl;
+
+    return 0;
+
+}
